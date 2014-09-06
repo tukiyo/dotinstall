5a140aaf18f1a0c7ddd2e67632b83fe85d3c3a2c
tukiyo3 <tukiyo3@gmail.com>
2014-09-06 11:15:58 +0900

    ansibleをroleを使って書き換えた。CentOS7対応

--- ansible/run.sh
+++
@@ -1,4 +0,0 @@
-#ansible-playbook -i hosts "yum_update/book.yml" #--ask-sudo-pass
-#ansible-playbook -i hosts "lamp/httpd.yml" #--ask-sudo-pass
-#ansible-playbook -i hosts "remi/book.yml" #--ask-sudo-pass
-ansible-playbook -i hosts "lamp/php.yml" #--ask-sudo-pass
