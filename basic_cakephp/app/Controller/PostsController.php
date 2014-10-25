78a1351334add95d653b0f76b378ce85c65f4667
tukiyo3 <tukiyo3@gmail.com>
2014-10-25 18:19:45 +0900

    CakePHP入門のソース追加

---
+++ basic_cakephp/app/Controller/PostsController.php
@@ -0,0 +1,58 @@
+<?php
+class PostsController extends AppController {
+
+	public $helpers = array('Html', 'Form');
+
+	public function index() {
+		$params = array(
+		    'order' => 'modified desc',
+		    'limit' => 50
+		);
+		$this->set('posts', $this->Post->find('all', $params));
+		$this->set('title', '記事一覧');
+	}
+	public function view($id = null) {
+		$this->Post->id = $id;
+		$this->set('post', $this->Post->read());
+	}
+	public function add() {
+		if ($this->request->is('post')) {
+			if ($this->Post->save($this->request->data)) {
+				$this->Session->setFlash('Success!');
+				$this->redirect(array('action'=>'index'));
+			} else {
+				$this->Session->setFlash('failed!');
+			}
+		}
+	}
+	public function edit($id = null) {
+		$this->Post->id = $id;
+		if ($this->request->is('get')) {
+			$this->request->data = $this->Post->read();
+		} else {
+			if ($this->Post->save($this->request->data)) {
+				$this->Session->setFlash('Success!');
+				$this->redirect(array('action'=>'index'));
+			} else {
+				$this->Session->setFlash('failed!');
+			}
+		}
+	}
+	public function delete($id = null) {
+		$this->Post->id = $id;
+		if ($this->request->is('get')) {
+			throw new MethodNotAllowedException();
+		}
+		if ($this->request->is('ajax')) {
+			if ($this->Post->delete($id)) {
+				$this->autoRender = false;
+				$this->autoLayout = false;
+				$response = array('id' => $id);
+				$this->header('Content-Type: application/json');
+				echo json_encode($response);
+				exit();
+			}
+		}
+		$this->redirect(array('action'=>'index'));
+	}
+}
---
+++ basic_cakephp/basic_cakephp.sql
@@ -0,0 +1,82 @@
+-- MySQL dump 10.14  Distrib 5.5.37-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: basic_cakephp
+-- ------------------------------------------------------
+-- Server version	5.5.37-MariaDB-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `commenter` varchar(255) DEFAULT NULL,
+  `body` text,
+  `post_id` int(11) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` VALUES (1,'c 1','b 1',22,'2014-10-25 17:35:26','2014-10-25 17:35:26'),(2,'c 2','b 2',22,'2014-10-25 17:35:26','2014-10-25 17:35:26'),(3,'c 3','b 3',22,'2014-10-25 17:35:26','2014-10-25 17:35:26'),(4,NULL,'sdf',22,'2014-10-25 18:11:19','2014-10-25 18:11:19'),(5,NULL,'いい',22,'2014-10-25 18:11:28','2014-10-25 18:11:28'),(6,'まま','ああ',22,'2014-10-25 18:12:17','2014-10-25 18:12:17');
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `body` text,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (22,'ほげ','ああ','2014-10-25 15:24:10','2014-10-25 15:24:10');
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-10-25 18:18:39
